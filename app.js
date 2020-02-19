8f901ca8386c87cf1496f9c6013993e79d456638
Aaron <ctli1993@gmail.com>
2020-02-19 13:43:50 +0800

    change the port

--- app.js
+++ app.js
@@ -8,7 +8,7 @@ const passport = require("./config/passport");
 const methodOverride = require("method-override");
 
 const app = express();
-const port = process.env.PORT || 3001;
+const port = process.env.PORT || 3000;
 if (process.env.NODE_ENV !== "production") {
   require("dotenv").config();
 }
