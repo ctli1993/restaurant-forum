4d12900801e588356da12f67eaf585a551f0b417
Aaron <ctli1993@gmail.com>
2020-02-23 19:53:36 +0800

    trying to fix sequelize stuff

--- app.js
+++ app.js
@@ -35,7 +35,7 @@ app.use((req, res, next) => {
 });
 
 app.listen(port, () => {
-  db.sequelize.sync();
+  db.sequelize.sync({force: true});
   console.log(`Example app listening on port ${port}`);
 });
 
