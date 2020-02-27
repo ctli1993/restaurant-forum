9dc6354c6f6f4cf48129ba26a6b1134dbf12c5f1
Aaron <ctli1993@gmail.com>
2020-02-28 00:53:12 +0800

    get categories API refactor

--- controllers/categoryController.js
+++ controllers/categoryController.js
@@ -1,18 +1,13 @@
 const db = require("../models");
 const Category = db.Category;
+const categoryService = require("../services/categoryService");
+
 let categoryController = {
   getCategories: (req, res) => {
-    return Category.findAll().then(categories => {
-    if(req.params.id) { 
-        Category.findByPk(req.params.id)
-        .then((category) => {
-            return res.render('admin/categories', JSON.parse(JSON.stringify({ categories: categories, category: category }))
-            )
-        })
-    } else
+    categoryService.getCategories(req, res, data => {
       return res.render(
         "admin/categories",
-        JSON.parse(JSON.stringify({ categories: categories }))
+        JSON.parse(JSON.stringify({ data }))
       );
     });
   },
@@ -32,28 +27,23 @@ let categoryController = {
 
   putCategory: (req, res) => {
     if (!req.body.name) {
-      req.flash('error_messages', 'name didn\'t exist')
-      return res.redirect('back')
+      req.flash("error_messages", "name didn't exist");
+      return res.redirect("back");
     } else {
-      return Category.findByPk(req.params.id)
-        .then((category) => {
-          category.update(req.body)
-            .then((category) => {
-              res.redirect('/admin/categories')
-            })
-        })
+      return Category.findByPk(req.params.id).then(category => {
+        category.update(req.body).then(category => {
+          res.redirect("/admin/categories");
+        });
+      });
     }
   },
 
   deleteCategory: (req, res) => {
-    return Category.findByPk(req.params.id)
-      .then((category) => {
-        category.destroy()
-          .then((category) => {
-            res.redirect('/admin/categories')
-          })
-      })
+    return Category.findByPk(req.params.id).then(category => {
+      category.destroy().then(category => {
+        res.redirect("/admin/categories");
+      });
+    });
   }
-
 };
 module.exports = categoryController;
